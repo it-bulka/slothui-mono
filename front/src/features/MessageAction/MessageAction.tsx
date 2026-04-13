8777820994d52576e6db7dba448545caaa8859c4
IT.bulka <i.it.bulka@gmail.com>
2026-04-13 12:54:39 +0200

    front/feat(nav): add navigation to chats and settings actions

--- front/src/features/MessageAction/MessageAction.tsx
+++ front/src/features/MessageAction/MessageAction.tsx
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router';
 import { ActionButton } from '@/shared/ui';
 import MessageSvg from '@/shared/assets/images/sidebar/message.svg?react'
+import { AppRoutes, RoutePaths } from '@/shared/config/routeConfig/routeConfig';
 
 export const MessageAction = () => {
-  return <ActionButton variant="secondary" Icon={MessageSvg}/>
+  const navigate = useNavigate();
+
+  return <ActionButton variant="secondary" Icon={MessageSvg} onClick={() => navigate(RoutePaths[AppRoutes.CHATS])}/>
 }
--- front/src/features/SettingAction/SettingAction.tsx
+++ front/src/features/SettingAction/SettingAction.tsx
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router';
 import { ActionButton } from '@/shared/ui';
 import SettingSvg from '@/shared/assets/images/sidebar/6.settings.svg?react'
+import { AppRoutes, RoutePaths } from '@/shared/config/routeConfig/routeConfig';
 
 export const SettingAction = () => {
-  return <ActionButton variant="secondary" Icon={SettingSvg}/>
+  const navigate = useNavigate();
+
+  return <ActionButton variant="secondary" Icon={SettingSvg} onClick={() => navigate(RoutePaths[AppRoutes.SETTINGS])}/>
 }
