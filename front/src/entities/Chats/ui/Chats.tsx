3ab47b6b61c4e5cbfd2e07139ec8401e46e80cb3
IT.bulka <i.it.bulka@gmail.com>
2026-04-16 11:16:44 +0200

    front/feat(chats): show unread message count badge per chat

--- front/src/entities/Chats/ui/Chats.tsx
+++ front/src/entities/Chats/ui/Chats.tsx
@@ -2,9 +2,11 @@ import { List } from '@/shared/ui';
 import { type Chat } from '../model/types/chat.type.ts';
 import { ChatRow } from './ChatRow.tsx';
 import { useGoToChat } from '@/entities/Chats/model/hooks/useGoToChat.tsx';
+import { useUnreadMessagesByChatSelect } from '@/entities/NotificationsCounters';
 
 export const Chats = ({ chats, className }: { chats: Chat[], className?: string }) => {
- const { goToChat } = useGoToChat()
+  const { goToChat } = useGoToChat()
+  const unreadByChat = useUnreadMessagesByChatSelect()
 
   return (
     <List className={className}>
@@ -15,6 +17,7 @@ export const Chats = ({ chats, className }: { chats: Chat[], className?: string 
           avatar={chat.avatarUrl}
           messagePreview={chat.lastMessage?.text}
           onClick={goToChat(chat.id)}
+          unreadCount={unreadByChat[chat.id] || undefined}
         />
       ))}
     </List>
