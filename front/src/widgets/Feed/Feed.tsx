c0a7b4dd29d9b6fecff3baaa39070bc70df8e68a
IT.bulka <i.it.bulka@gmail.com>
2026-04-21 12:28:13 +0200

    front/fix(feed): make header sticky with correct scroll runway

--- front/src/widgets/Feed/Feed.tsx
+++ front/src/widgets/Feed/Feed.tsx
@@ -1,8 +1,12 @@
 import { type ReactNode } from 'react';
 export const Feed = ({ header, children }: { header?: ReactNode, children: ReactNode }) => {
   return (
-    <div className="flex flex-col h-full">
-      {header}
+    <div className="flex flex-col min-h-full">
+      {header && (
+        <div className="sticky top-0 z-10000000 bg-light-l3">
+          {header}
+        </div>
+      )}
       <div className="bg-white px-main py-6 grow flex flex-col gap-4">
         {children}
       </div>
