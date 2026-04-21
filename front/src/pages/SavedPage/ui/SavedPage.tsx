57faf29bbe0691155731979d4a5afdf093cf6d18
IT.bulka <i.it.bulka@gmail.com>
2026-04-21 13:29:15 +0200

    front/refactor(pages): improve Liked/Saved pages structure using Feed header

--- front/src/pages/SavedPage/ui/SavedPage.tsx
+++ front/src/pages/SavedPage/ui/SavedPage.tsx
@@ -12,32 +12,28 @@ const SavedPage = memo(() => {
   const { activeIndex, onTabChange } = useTypeTab(TABS);
 
   return (
-    <Feed>
-      <main className="relative flex flex-col min-h-full">
-        <Typography
-          variant="h1"
-          type={TypographyTypes.BLOCK_TITLE}
-          bold
-          className="toolbar sticky top-0 z-10"
-        >
-          Saved
-        </Typography>
-
-        <section className="flex flex-col flex-1">
-          <Tab
-            scrollableTabs
-            animated
-            tabs={['Posts', 'Events']}
-            activeTabIndex={activeIndex}
-            onTabChange={onTabChange}
-            contents={[
-              <SavedPostsContent />,
-              <SavedEventsContent />,
-            ]}
-            contentClassName="space-y-4 px-main py-main"
-          />
-        </section>
-      </main>
+    <Feed header={(
+      <Typography
+        variant="h1"
+        type={TypographyTypes.BLOCK_TITLE}
+        bold
+        className="toolbar sticky top-0 z-10"
+      >
+        Saved
+      </Typography>
+    )}>
+      <Tab
+        scrollableTabs
+        animated
+        tabs={['Posts', 'Events']}
+        activeTabIndex={activeIndex}
+        onTabChange={onTabChange}
+        contents={[
+          <SavedPostsContent />,
+          <SavedEventsContent />,
+        ]}
+        contentClassName="space-y-4 px-main py-main"
+      />
     </Feed>
   );
 });
