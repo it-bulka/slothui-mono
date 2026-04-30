d72d0fed0bc81450fe564bfb95611ac66e376f6c
IT.bulka <i.it.bulka@gmail.com>
2026-04-30 10:31:56 +0200

    front/fix(story-message): match story modal size to stories viewer

--- front/src/entities/Message/ui/content/StoryMessage/StoryMessageModal.tsx
+++ front/src/entities/Message/ui/content/StoryMessage/StoryMessageModal.tsx
@@ -1,6 +1,5 @@
 import { Avatar, Modal } from '@/shared/ui';
 import { Story, type StoryProps } from '../../../../Story';
-
 import { getUserPage } from '@/shared/config/routeConfig/routeConfig.tsx';
 import { Link } from 'react-router';
 import { Typography, CloseButton } from '@/shared/ui';
@@ -15,19 +14,22 @@ type StoryMessageModal = StoryProps & {
     username: string
   }
 }
+
 export const StoryMessageModal = ({
   type, url, onComplete, id, isOpen, onClose, user
 }: StoryMessageModal) => {
   return (
-    <Modal onClose={onClose} isOpen={isOpen} aria-label="Story preview dialog">
-      <div className="absolute top-2 left-0 w-full z-10 flex justify-between gap-2 px-2">
-        <Link to={getUserPage(user.id)} className="flex items-center">
-          <Avatar src={user.avatarUrl} />
-          <Typography>{user.nickname}</Typography>
-        </Link>
-        <CloseButton onClick={onClose} />
+    <Modal onClose={onClose} isOpen={isOpen} fit aria-label="Story preview dialog">
+      <div className="relative h-[80vh] max-w-[80vh] aspect-[3/4] overflow-hidden rounded-2xl bg-black">
+        <div className="absolute top-2 left-0 w-full z-20 flex justify-between items-center gap-2 px-2">
+          <Link to={getUserPage(user.id)} className="flex items-center gap-2">
+            <Avatar src={user.avatarUrl} />
+            <Typography>{user.nickname}</Typography>
+          </Link>
+          <CloseButton onClick={onClose} />
+        </div>
+        <Story type={type} url={url} onComplete={onComplete} id={id} />
       </div>
-      <Story type={type} url={url} onComplete={onComplete} id={id} />
     </Modal>
   )
 }
