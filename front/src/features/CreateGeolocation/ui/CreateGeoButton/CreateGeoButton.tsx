09688a57dcd3e11d725b6efeb15b0966e9e32b38
IT.bulka <i.it.bulka@gmail.com>
2026-05-23 09:08:16 +0200

    feat(CreateGeoButton): preload geo chunks on idle for faster mobile open

--- front/src/features/CreateGeolocation/ui/CreateGeoButton/CreateGeoButton.tsx
+++ front/src/features/CreateGeolocation/ui/CreateGeoButton/CreateGeoButton.tsx
@@ -1,11 +1,21 @@
 import { ActionButton } from '@/shared/ui/ActionButton';
 import GeoSvg from '@/shared/assets/images/actions/geo.svg?react'
 import { CreateGeoModal } from '../CreateGeoModal/CreateGeoModal.tsx';
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import { useGeoDraft } from '@/features/CreateGeolocation/model';
 
 export const CreateGeoButton = memo(() => {
   const { isOpen, open, close } = useGeoDraft()
+
+  useEffect(() => {
+    const preload = () => {
+      import('../CreateGeoForm/CreateGeoForm')
+      import('@/entities/Map/ui/MapView/MapView')
+    }
+    const ric = window.requestIdleCallback ?? ((cb: () => void) => setTimeout(cb, 200))
+    const id = ric(preload)
+    return () => window.cancelIdleCallback?.(id)
+  }, [])
 
   return (
     <>
