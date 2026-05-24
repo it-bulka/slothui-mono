6ee89aaf001d793b3569927de7db90ba849015ae
IT.bulka <i.it.bulka@gmail.com>
2026-05-24 17:02:26 +0200

    back/fix(IsStrongPassword): reduce max password length

--- back/src/common/validators/is-strong-password.decorator.ts
+++ back/src/common/validators/is-strong-password.decorator.ts
@@ -5,7 +5,7 @@ export function IsStrongPassword() {
   return applyDecorators(
     IsString(),
     MinLength(6, { message: 'Password is too short (min 6)' }),
-    MaxLength(30, { message: 'Password is too long (max 30)' }),
+    MaxLength(20, { message: 'Password is too long (max 20)' }),
     Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).+$/, {
       message:
         'Password must contain uppercase, lowercase, special character and no spaces',
